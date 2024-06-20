eebe178e18c272fd50210968ce00b1b3d2450586
Noah Mercer <noah.mercer@gmail.com>
2024-06-20 15:02:54 -0700

    Make pay-info conform to spec

--- common/pay-info/pay-info.js
+++ common/pay-info/pay-info.js
@@ -31,22 +31,33 @@ export class Payboard {
             for (let i=0; i<earnings.length; i++) {
                 const curEarn = earnings[i];
                 overallTotal += curEarn.amount;
-                const dayData = data[curEarn.date] || {};
+                const ymdDate = curEarn.date.substring(0, 10);
+                const dayData = data[ymdDate] || {};
                 const earnType = curEarn.type;
-                if (earnType === earningsTypes.BREATH1) {
-                    dayData['session1Earned']= curEarn.amount;
-                } else if (earnType === earningsTypes.BREATH2) {
-                    dayData['session2Earned']= curEarn.amount;
-                } else if (earnType === earningsTypes.STREAK_BONUS1 || earnType === earningsTypes.STREAK_BONUS2) {
-                    dayData['streakBonus']= curEarn.amount;
+                if (earnType === earningsTypes.VISIT_1 || earnType === earningsTypes.VISIT_2) {
+                    dayData['visits'] = (dayData['visits'] || 0) + curEarn.amount;
+                } else if (earnType === earningsTypes.BREATH1) {
+                    dayData['sessions'] = (dayData['sessions'] || 0) + curEarn.amount;
+                } else if (earnType === earningsTypes.PERFORMANCE_BREATH2) {
+                    // participants get $8 for PERFORMANCE_BREATH2, but $4 of 
+                    // that is considered normal session pay and half is a bonus
+                    // for doing two sessions in a day
+                    dayData['sessions'] = (dayData['sessions'] || 0) + curEarn.amount / 2;
+                    dayData['bonuses'] = (dayData['bonuses'] || 0) + curEarn.amount / 2;
+                } else if (earnType === earningsTypes.TOP_25 ||
+                    earnType === earningsTypes.TOP_66 ||
+                    earnType === earningsTypes.COMPLETION_BREATH2 ||
+                    earnType === earningsTypes.STREAK_BONUS
+                ) {
+                    dayData['bonuses'] = (dayData['bonuses'] || 0) + curEarn.amount;
                 }
                 const total = Object.keys(dayData).filter(k => k !== 'total').reduce((sum, key) => sum + dayData[key], 0);
                 dayData['total'] = total;
-                data[curEarn.date] = dayData;
+                data[ymdDate] = dayData;
             }
             const templateData = Object.entries(data).map(([k, v]) => {
                 const dateParts = k.split('-');
-                v['day'] = `${dateParts[1]}/${dateParts[2]}/${dateParts[0]}`; // conver YYYY-MM-DD to MM/DD/YYYY
+                v['day'] = `${dateParts[1]}/${dateParts[2]}/${dateParts[0]}`; // convert YYYY-MM-DD to MM/DD/YYYY
                 return v
             });
 
