1ee235d8a377ee0a354fb5a0f09e49c7fab14521
Noah Mercer <noah.mercer@gmail.com>
2024-06-20 21:29:16 -0700

    Revert "Turns emwave hiding on"
    This reverts commit 22f539c8c637edaccbf5953aaed6a6065912cdfb.

--- client/src/emwave.js
+++ client/src/emwave.js
@@ -159,17 +159,17 @@ export default {
             emWavePid = await (spawn('/Applications/emWave Pro.app/Contents/MacOS/emWaveMac', [], {stdio: 'ignore'})).pid
         } else if (process.platform === 'win32') {
             emWavePid = await (spawn('C:\\Program Files\\HeartMath\\emWave\\emWavePC.exe', [], {stdio: 'ignore'})).pid
-            const startScript = process.env.NODE_ENV === 'production' ? path.join(path.dirname(app.getPath('exe')), 'start-emwave-hidden.ps1') : path.join(app.getAppPath(), '../src/powershell/start-emwave-hidden.ps1')
-            const emWaveProc = spawn('C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe', ["-executionPolicy", "bypass", "-file", startScript])
-            emWaveProc.stdout.on("data", (data) => {
-                const dataStr = data.toString().trim()
-                if (dataStr.length > 0) {
-                    emWavePid = dataStr
-                }
-            });
-            emWaveProc.stderr.on("data", (data) => {
-                console.error("powershell stderr: " + data)
-            });
+            // const startScript = process.env.NODE_ENV === 'production' ? path.join(path.dirname(app.getPath('exe')), 'start-emwave-hidden.ps1') : path.join(app.getAppPath(), '../src/powershell/start-emwave-hidden.ps1')
+            // const emWaveProc = spawn('C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe', ["-executionPolicy", "bypass", "-file", startScript])
+            // emWaveProc.stdout.on("data", (data) => {
+            //     const dataStr = data.toString().trim()
+            //     if (dataStr.length > 0) {
+            //         emWavePid = dataStr
+            //     }
+            // });
+            // emWaveProc.stderr.on("data", (data) => {
+            //     console.error("powershell stderr: " + data)
+            // });
         } else {
             throw `The '${process.platform}' operating system is not supported. Please use either Macintosh OS X or Windows.`
         }
