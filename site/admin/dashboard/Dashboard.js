fb4c2fcbfdb8eaf768a4b290534e06c4d6568f0b
Noah Mercer <noah.mercer@gmail.com>
2024-06-27 15:39:52 -0700

    Gets dashboard working

--- site/admin/dashboard/Dashboard.js
+++ site/admin/dashboard/Dashboard.js
@@ -1,8 +1,12 @@
 import tableTmpl from "./templates/table.handlebars";
 import userDetailsTmpl from "./templates/userDetails.handlebars";
-import statusTmpl from "./templates/status.handlebars";
-import { DatedCheckbox } from "./DatedCheckbox";
 import { Payboard } from "../../../common/pay-info/pay-info";
+import { statusTypes } from "../../../common/types/types";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc.js";
+import timezone from "dayjs/plugin/timezone.js";
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 export class Dashboard {
     constructor(tbody, userDetailsDiv, apiClient) {
@@ -17,43 +21,11 @@ export class Dashboard {
         await this.loadUsers('active'); // defaults to active only
         this.tbody.addEventListener("click", async event => {
             const target = event.target;
-            if (target.type == "checkbox") {
-                await this.handleCheckboxEvent(event);
-            } else if (target.className == "username") {
+            if (target.className == "username") {
                await this.handleUserEvent(event);
             }
             return;
         });
-
-        this.tbody.addEventListener("change", async event => {
-            const target = event.target;
-            if (target.type == "datetime-local") {
-                await this.handleDateFieldEvent(event);
-            }
-        });
-
-        const debounce = (callback, waitTime) => {
-            let timer
-            return (...args) => {
-                clearTimeout(timer)
-                timer = setTimeout(() => callback(...args),waitTime);
-            }
-        }
-
-        this.tbody.addEventListener("input", debounce(async (event) => {
-            event.target.classList.remove('error');
-            const userId = event.target.closest("tr")?.dataset.userId;
-            const participantId = event.target.textContent;
-            if (!this.isValidParticipantId(participantId)) {
-                event.target.classList.add('error');
-                alert(`${participantId} is not a valid participant id.`);
-                return;
-            }
-
-            await this.apiClient.updateUser(userId, {participantId: participantId});
-            const user = this.users[userId];
-            user['participantId'] = participantId;
-        }, 1500));
 
         this.userDetailsDiv.addEventListener("click", async event => {
             await this.handleDetailsClickEvent(event)
@@ -83,10 +55,6 @@ export class Dashboard {
         this.fetchStatusForUsers();
     }
 
-    isValidParticipantId(id) {
-        return id && id.match(/[0-9]{4}[A-z]?/);
-    }
-
     async loadUsers(userType) {
         if (userType !== 'active' && userType !== 'all') {
             throw new Error(`Expected userType to be 'active' or 'all', but got '${userType}'.`);
@@ -102,13 +70,9 @@ export class Dashboard {
             this.users[u.userId] = u;
             displayInfo.push(
                 {
-                    userId: u.userId,
-                    participantId: u.participantId,
                     name: u.name,
-                    email: u.email,
-                    phone: u.phone,
-                    dropped: new DatedCheckbox('dropped', u.progress ? u.progress.dropped : null),
-                    inactive: u.progress && (u.progress.dropped || u.progress.stage3Complete)
+                    userId: u.userId,
+                    inactive: !u.progress?.status || u.progress.status != statusTypes.ACTIVE
                 }
             );
         }
@@ -116,68 +80,21 @@ export class Dashboard {
         this.tbody.innerHTML = tableTmpl({users: displayInfo});
     }
 
-    async handleCheckboxEvent(event) {
-        const {key, date, origDateStr} = DatedCheckbox.handleClick(event);
-        const checkbox = event.target;
-        checkbox.disabled = true;
-        const userId = checkbox.closest("tr")?.dataset.userId;
-        try {
-            const dateStr = date ? date.format("YYYY-MM-DDTHH:mm:ssZ") : null;
-            await this.updateUserProgress(userId, key, dateStr);
-        } catch (err) {
-            DatedCheckbox.undoClick(event, origDateStr);
-            console.error(`Error setting date for ${key} for ${userId}`, err);
-            window.alert("A problem ocurred. Please try again later.");
-            event.preventDefault();
-        } finally {
-            checkbox.disabled = false;
-        }
-    }
-
-    async handleDateFieldEvent(event) {
-        const dateField = event.target;
-
-        const {key, datetime} = FutureDateField.handleChange(event);
-        const userId = dateField.closest("tr")?.dataset.userId;
-        dateField.disabled = true;
-        try {
-            await this.updateUserProgress(userId, key, datetime.format("YYYY-MM-DDTHH:mm:ssZ"));
-        } catch (err) {
-            event.preventDefault();
-            console.error(`Error setting visit 2 scheduled date for ${userId}`, err);
-            window.alert("A problem ocurred. Please try again later.");
-            const user = this.users[userId];
-            const origDateStr = user.progress ? user.progress.visit2Scheduled : null;
-
-            // this is stupid, but for some reason (webpack bug?)
-            // setting dateField.value directly here will trigger
-            // a new change event
-            setTimeout(() => dateField.value = origDateStr, 100)
-        } finally {
-            dateField.disabled = false;
-        }
-    }
-
-    async updateUserProgress(userId, key, value) {
-        await this.refreshUser(userId);
-        const user = this.users[userId];
-        const progress = user.progress ?? {};
-        if (value) {
-            progress[key] = value;
-        } else {
-            progress[key] = null;
-        }
-        await this.apiClient.updateUser(userId, {progress});
-    }
-
     async handleUserEvent(event) {
         const parentRow = event.target.closest("[data-user-id]");
         const userId = parentRow.dataset.userId;
         const user = this.users[userId];
+        const today = dayjs().tz('America/Los_Angeles');
+        // status is based on past three days, starting yesterday
+        const statusStart = today.subtract(4, 'days').startOf('day');
+        const statusEnd = today.subtract(1, 'days').endOf('day');
+        const dateRange = `${statusStart.format('MM/DD/YYYY')}-${statusEnd.format('MM/DD/YYYY')}`;
         const dispUser = {
             userId: user.userId,
             phone: user.phone_number,
-            email: user.email
+            email: user.email,
+            status: user.status,
+            dateRange: dateRange
         };
 
         this.userDetailsDiv.innerHTML = userDetailsTmpl({user: dispUser});
@@ -203,15 +120,31 @@ export class Dashboard {
     }
 
     async fetchStatusForUsers() {
-        for (const userId of Object.keys(this.users)) {
-            const status = await this.apiClient.getUserStatus(userId);
-            const userRow = document.querySelectorAll(`[data-user-id="${userId}"]`)[0]; // TODO handle case where we don't find the user row
-            const startDateCell = userRow.querySelectorAll(".start-date")[0];
-            startDateCell.innerHTML = status.startDate;
-            const status2Cell = userRow.querySelectorAll(".status2")[0];
-            status2Cell.innerHTML = statusTmpl({status: status.status2, total: 12});
-            const status3Cell = userRow.querySelectorAll(".status3")[0];
-            status3Cell.innerHTML = statusTmpl({status: status.status3, total: 48});
+        for (const u of Object.values(this.users)) {
+            const userRow = document.querySelectorAll(`[data-user-id="${u.userId}"]`)[0]; // TODO handle case where we don't find the user row
+            const statusCell = userRow.querySelectorAll(".status")[0];
+            statusCell.innerHTML = '';
+            if (u.progress?.status !== statusTypes.ACTIVE) {
+                if (!u.progress?.status) {
+                    statusCell.classList.add("dot", "empty");
+                } else if (u.progress.status === statusTypes.DROPPED) {
+                    statusCell.classList.add("dot", "gray");
+                } else if (u.progress.status === statusTypes.COMPLETE) {
+                    statusCell.classList.add("dot", "blue");
+                }
+                return;
+            }
+            
+
+            const status = await this.apiClient.getUserStatus(u.userId);
+            u.status = status;
+            if (status.recentMinutes >= 72) {
+                statusCell.classList.add("dot", "green");
+            } else if (status.recentMinutes >= 36 && status.recentMinutes <= 71) {
+                statusCell.classList.add("dot", "yellow");
+            } else {
+                statusCell.classList.add("dot", "red");
+            }
         }
     }
 
